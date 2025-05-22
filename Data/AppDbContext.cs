c101e0e92756c509300f2400e5f9bf95073b8dd6
Jjeff2002 <jeffinjitto2002@gmail.com>
2025-05-22 14:40:18 +0530

    FirstMigration-Clients and Locations

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿// AppDbContext.cs
 using Microsoft.EntityFrameworkCore;
+using RecruitX.Models;
 
 public class AppDbContext : DbContext
 {
@@ -7,5 +8,33 @@ public class AppDbContext : DbContext
         : base(options)
     {
     }
+
+    public DbSet<Clients> Clients { get; set; }
+    public DbSet<Locations> Locations { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Clients>(entity =>
+        {
+            entity.ToTable("clients");
+
+            entity.HasKey(e => e.Client_Id);
+            entity.Property(e => e.Client_Id).HasColumnName("client_id");
+            entity.Property(e => e.Client_Name).HasColumnName("client_name").IsRequired();
+            entity.Property(e => e.Client_Country).HasColumnName("client_country");
+        });
+
+        modelBuilder.Entity<Locations>(entity =>
+        {
+            entity.ToTable("locations");
+
+            entity.HasKey(e => e.Location_Id);
+            entity.Property(e => e.Location_Id).HasColumnName("client_id");
+            entity.Property(e => e.Location_Name).HasColumnName("client_name").IsRequired();
+            entity.Property(e => e.Country).HasColumnName("client_country");
+        });
+    }
+
+
 
 }
